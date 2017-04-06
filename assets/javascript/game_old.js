2deb121cf3850e9d1939bbcaa6ce559419e310f7
jldoucette <jd@jonathandoucette.com>
2017-04-05 19:27:42 -0700

    with corrected names

--- assets/javascript/game_old.js
+++ assets/javascript/game_old.js
@@ -5,41 +5,32 @@ var guesses=0;
 // var playerLetter="+";
 var isValidLetter=false;
 var isWordLetter=null;
-var arrWords=["passport","ticket","border","tours","customs","luggage","airplane","trains","hotel","bellboy","beach"];
+var arrWords=["passport","ticket","border","tours","customs","luggage","airplane","trains","hotel","bellboy","rentalcar"];
 var arrValidLetters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var arrGuessedLetters=[];
 var arrCorrectGuess=[];
 var arrActiveWord=[];
-var arrActiveWordNoDup=[];
+var arrActiveWordNoDup=[]
 var screenStatus="";
 var guessedLettersDisplay="";
 var indexofGuessedLetters=1;
 var lettersCountGuessed=0;
 var numGuessesAllowed=10;
 var newGuess=false;
+// var actualGuess=true;
+
 
 var randomWord=arrWords[Math.floor(Math.random() * arrWords.length)];
 var currentWord=randomWord;
 
-var screenHTML = "<h2>Press any key start playing!</h2>" +
+var screenHTML = "<h3>Press any key start playing!</h3>" +
          "<h4>Solved Words (Wins): " + wins + "</h4>" +
          "<h4>Words out of guesses: " + losses + "</h4>";
-var guessRemainStatus=
-		"<h2> Remaining Guesses: " + numGuessesAllowed + "</h2>";
-		// document.querySelector("#remainingGuesses").innerHTML = guessRemainStatus;         
-var guessedLettersStatus=
-		"<h2> Guessed Letters: " + guessedLettersDisplay + "</h2>";
-	
-		
-		
-// document.querySelector("#game").innerHTML=screenHTML;
+         // "<h4>Guessed Letters:  " + arrGuessedLetters[0].toUpperCase() + "</h5>";
 
-function initializeGame() {
-document.querySelector("#remainingGuesses").innerHTML = guessRemainStatus;         
-document.querySelector("#game").innerHTML=screenHTML;
-document.querySelector("#guessedStatus").innerHTML = guessedLettersStatus;
-ingestWord();
-}
+         // document.querySelector("#game").innerHTML = screenHTML;
+// var screenStatus = "<h4>Guessed Letters:" + arrGuessedLetters[indexofGuessedLetters].toUpperCase() + "</h4>";
+      
 	
 
 function ingestWord() {
@@ -54,13 +45,10 @@ function ingestWord() {
 				console.log("Revised: " + arrActiveWordNoDup.length); 
 				console.log("ingestWord: Completed");
 				displayStatus();
-
+}
 
 	document.onkeyup = function(event) {
-		var screenHTML = "<h2>Game in progress! Good Luck on your travels!</h2>" +
-         "<h4>Solved Words (Wins): " + wins + "</h4>" +
-         "<h4>Words out of guesses: " + losses + "</h4>";
-			document.querySelector("#game").innerHTML=screenHTML;
+
 				       var playerInput = event.key;
 				       var playerLetter = playerInput.toLowerCase();
 				// console.log(playerInput);
@@ -79,7 +67,6 @@ function ingestWord() {
 		}
 		else	{
 			var isValidLetter=false;
-			var newGuess=false;
 			console.log("CheckInput if: Not a Valid Letter");
 			displayStatus();
 			displayGuessedLetters();
@@ -103,13 +90,11 @@ function ingestWord() {
 			// console.log(arrValidLetters.indexOf(playerLetter));
 			// console.log(playerLetter);
 			var isWordLetter=true;
-			
 			var realWordLetter=1;
 			console.log("just set isWordLetter to true, Index Value: " + arrActiveWord.indexOf(playerLetter) + " " + playerLetter + " " + arrActiveWord);
 				}
 				else {
 						var isWordLetter=false;
-						// var newGuess=
 						console.log("Failure of playerLetter in arrActiveWord just set isWordLetter to " + isWordLetter);
 						checkLetter(isWordLetter);
 						checkRemainingGuesses(isWordLetter,newGuess);
@@ -145,13 +130,24 @@ function ingestWord() {
 				displayStatus();
 				displayGuessedLetters();
 
+
 			}
-		
+		}
 	
 
+function checkRemainingGuesses(isWordLetter,newGuess) {
+	console.log("In Function checkRemainingGuesses");
+	if ((isWordLetter==false) && (newGuess)) {
+			numGuessesAllowed--;
+			console.log("Allowed Guesses set to " + numGuessesAllowed);
+			console.log("value of isWordLetter on CheckRemainingGuesses is " + isWordLetter);
+			}
+			else {
+				console.log("checkRemainingGuesses failed test " + isWordLetter + " " + newGuess);
+			}
+}
 
-}
-}
+
 
 
 function displayStatus() {
@@ -168,7 +164,7 @@ function displayStatus() {
 				// console.log("displayStatus:Got to lower display Status-Correct Guess");
 			
 	
-				// checkSolved();
+				checkSolved();
 				}
 				
 			else {
@@ -182,9 +178,9 @@ function displayStatus() {
 				// console.log("displayStatus:Got to lower display Status-Not Guessed");
 				
 				}
-				
+
 	}
-				checkSolved();
+
 
 	}
 	function checkSolved() {
@@ -195,119 +191,29 @@ function displayStatus() {
 		if(arrCorrectGuess.length==arrActiveWordNoDup.length) {
 					console.log("SOLVED!");
 					var puzzleSolved=true;
-					var gameWin=true;
-					console.log("Set gameWin to true");
 					wins++;
-					console.log("Increase wins by one");
 					console.log(puzzleSolved + " " + wins);
-					restartGame(gameWin);
-					console.log("Returned from restartGame");
+					restartGame();
 				}		
 	}
-	// displayGuessedLetters();
 
-function checkRemainingGuesses(isWordLetter,newGuess) {
-	console.log("In Function checkRemainingGuesses");
-
-	if ((isWordLetter==false) && (newGuess)) {
-			numGuessesAllowed--;
-			console.log("Allowed Guesses set to " + numGuessesAllowed);
-			console.log("value of isWordLetter on CheckRemainingGuesses is " + isWordLetter);
-			}
-			else {
-				console.log("checkRemainingGuesses failed test " + isWordLetter + " " + newGuess);
-			}
-			if (numGuessesAllowed==0) {
-				losses++;
-				var gameWin=false;
-				restartGame(gameWin);
-			}
-			
-	var guessRemainStatus=
-		"<h2> Remaining Guesses: " + numGuessesAllowed + "</h2>";
-		document.querySelector("#remainingGuesses").innerHTML = guessRemainStatus;		
-	}
-
+	
 	function displayGuessedLetters() {
-
 	var guessedLettersDisplay="";
 	for (k = 0; k < arrGuessedLetters.length; k++) {
 		var guessedLettersDisplay=guessedLettersDisplay + " " + arrGuessedLetters[k].toUpperCase();
 		// console.log(guessedLettersDisplay);
 		var guessedLettersStatus=
 		"<h2> Guessed Letters: " + guessedLettersDisplay + "</h2>";
-	
-		
 		document.querySelector("#guessedStatus").innerHTML = guessedLettersStatus;
-		console.log("displayGuessedLetters: Guessed Letters");
+		// console.log("displayGuessedLetters: Guessed Letters");
 		}
 	}
-
-// 	function checkRemainingGuesses(isWordLetter,newGuess) {
-// 	console.log("In Function checkRemainingGuesses");
-// 	if ((isWordLetter==false) && (newGuess)) {
-// 			numGuessesAllowed--;
-// 			console.log("Allowed Guesses set to " + numGuessesAllowed);
-// 			console.log("value of isWordLetter on CheckRemainingGuesses is " + isWordLetter);
-// 			}
-// 			else {
-// 				console.log("checkRemainingGuesses failed test " + isWordLetter + " " + newGuess);
-// 			}
-		
-
-// }
-function restartGame(gameWin) {
-	if (gameWin) {
+function restartGame() {
 						var gameStatus=
-					"<h2> Congrats! You Got the word!</h2>" +
-					"<h2> Press any key to keep playing!</h2";
-
-						var winningImage=
-						"<img src='assets/images/" + currentWord + ".jpg' alt="+ currentWord +">";	
+					"<h2> Congrats! You Got the word!</h2>";
+		document.querySelector("#roundstatus").innerHTML = gameStatus;
 					}
-					else {
-						var gameStatus="<h2>Oops...you ran out of guesses!</h2>";
-					
-						var winningImage=
-						"<h2 class='loseLanguage'> Sorry, you ran out of guesses and lost. Try again!</h2>"+
-					"<h2 class='loseLanguage'> Press any key to keep playing!</h2";
-					}
-			document.querySelector("#roundstatus").innerHTML = gameStatus;
-			document.querySelector("#resultpicture").innerHTML = winningImage;
-
-			document.onkeyup = function(event) {
-	
-				       var playanotherround = event.key;
-				       	var gameStatus="";
-				       	var winningImage="";
-				document.querySelector("#roundstatus").innerHTML = gameStatus;
-				document.querySelector("#resultpicture").innerHTML = winningImage;
-			reInitializeGame();
-				}
-			}
-function reInitializeGame() {
-window.guesses=0;
-window.isValidLetter=false;
-window.isWordLetter=null;
-window.arrGuessedLetters=[];
-window.arrCorrectGuess=[];
-window.arrActiveWord=[];
-window.arrActiveWordNoDup=[];
-window.screenStatus="";
-window.guessedLettersDisplay="";
-window.indexofGuessedLetters=1;
-window.lettersCountGuessed=0;
-window.numGuessesAllowed=10;
-window.newGuess=false;
-window.randomWord=arrWords[Math.floor(Math.random() * arrWords.length)];
-window.currentWord=randomWord;
-window.screenHTML = "<h3>Press any key start playing!</h3>" +
-         "<h4>Solved Words (Wins): " + wins + "</h4>" +
-         "<h4>Words out of guesses: " + losses + "</h4>";
-         document.querySelector("#game").innerHTML = screenHTML;
-         initializeGame();
-}
-
 
 
 function eliminateDuplicates(arr) {
